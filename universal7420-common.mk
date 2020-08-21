fd03ce7b564aed7c497fdb4dc741129fba38ca7a
xSil3nt <ahmedshazin21@gmail.com>
2020-08-21 10:50:37 +0500

    universal7420: Build CustomDoze & enable AOD support

--- universal7420-common.mk
+++ universal7420-common.mk
@@ -375,10 +375,12 @@ PRODUCT_PACKAGES += \
 #
 PRODUCT_PACKAGES += \
 	android.hardware.renderscript@1.0-impl
-
-# SamsungDoze
-#PRODUCT_PACKAGES += \
-#    SamsungDoze
+	
+#
+# CustomDoze
+#
+PRODUCT_PACKAGES += \
+	CustomDoze
 
 #
 # Security
